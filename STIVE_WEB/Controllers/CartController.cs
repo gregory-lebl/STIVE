766ad72e9b99dc3e62fcb7f31e45472ddd7a07e4
Gregory <gregory.leblond.dev@gmail.com>
2022-01-04 05:03:24 +0100

    Création CartController

---
+++ STIVE_WEB/Controllers/CartController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STIVE_WEB.Controllers
+{
+    public class CartController : Controller
+    {
+        /// <summary>
+        /// Affiche le panier contenant tous les articles
+        /// </summary>
+        public IActionResult Checkout()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("Cart")))
+            {
+                return View();
+            }
+            else
+            {
+                var sessionData = HttpContext.Session.GetString("Cart");
+
+
+                return View();
+            }
+        }
+    }
+}
