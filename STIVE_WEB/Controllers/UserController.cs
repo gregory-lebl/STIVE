78271bbdc6a1b2ffeb6278dd45dc5f268d567e94
Gregory <gregory.leblond.dev@gmail.com>
2022-02-21 20:20:32 +0100

    Formulaire insertion Customer + front formulaire connexion

---
+++ STIVE_WEB/Controllers/UserController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using STIVE_WEB.Models.Users;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace STIVE_WEB.Controllers
+{
+    public class UserController : Controller
+    {
+        static HttpClient client = new HttpClient();
+        private string BaseUrl = "https://localhost:44395";
+        
+        public ActionResult LoginForm()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateNewCustomerAsync(IFormCollection collection, Customer customer)
+        {
+            Customer newCustomer = customer;
+
+            if(customer.Password != customer.PasswordVerify)
+            {
+                ViewData["errorMessage"] = "Les mots de passe ne correspondent pas";
+                return View("RegisterForm");
+            }
+            else
+            {
+                string endpointApi = BaseUrl + "/api/customer/new2";
+                var client = new HttpClient();
+
+                await client.PostAsJsonAsync(endpointApi, newCustomer);
+
+                ViewData["successMessage"] = "Votre compte a bien été créé, vous pouvez vous connecter";
+                return View("RegisterForm");
+            }
+        }
+
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- STIVE_WEB/Models/Users/Customer.cs
+++ STIVE_WEB/Models/Users/Customer.cs
@@ -2,12 +2,24 @@
 
 namespace STIVE_WEB.Models.Users
 {
-    public class Customer : User
+    public class Customer
     {
-        public string CustomerReference { get; set; }
 
-        public Customer(string LastName, string FirstName, string Email, string Password, string PhoneNumber, string Address, string Cp, string City) : base(LastName, FirstName, Email, Password, PhoneNumber, Address, Cp, City){
-        var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public string CustomerReference { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordVerify { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Cp { get; set; }
+        public string City { get; set; }
+
+        public Customer()
+        {
+            var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             var Charsarr = new char[12];
             var random = new Random();
 
@@ -18,7 +30,6 @@ namespace STIVE_WEB.Models.Users
             var customerRef = new String(Charsarr);
 
             CustomerReference = customerRef;
-        
         }
 
     }
